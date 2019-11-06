207fc0d2ad2b332e01e4b1bce1e0e7ffbc1de44e
Aluno do Inatel <aluno@local.inatel.br>
2019-11-06 10:16:31 -0300

    first commit

---
+++ javaCap14lang/src/javacap14lang/JavaCap14lang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacap14lang;
+
+/**
+ *
+ * @author aluno
+ */
+public class JavaCap14lang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
