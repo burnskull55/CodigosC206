d3923e2344623cf9cc14a737276cf5a2ae3ce959
Aluno do Inatel <aluno@local.inatel.br>
2019-11-06 10:52:14 -0300

    yasssss

--- javaCap14lang/src/javacap14lang/JavaCap14lang.java
+++ javaCap14lang/src/javacap14lang/JavaCap14lang.java
@@ -16,6 +16,25 @@ public class JavaCap14lang {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int x1,x2;
+        int y1,y2;
+        double aux1,aux2;
+        double resposta;
+        double pMediox,pMedioy;
+        x1 = 0;
+        x2 = 3;
+        y1 = 0;
+        y2 = 4;
+        
+        aux1 =  Math.pow((x2-x1),2);
+        aux2 =  Math.pow(y2-y1,2);
+        resposta = Math.sqrt(aux1+aux2);
+        
+        pMediox = (x1+x2)/2;
+        pMedioy = (y1+y2)/2;
+        
+        System.out.println(resposta);
+        System.out.println("("+pMediox+","+pMedioy+")");
     }
     
 }
